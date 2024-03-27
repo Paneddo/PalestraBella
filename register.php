1b685993c058e32ccd8a1428fee42c0034d7e047
Paneddo <lorenzopanilorenzo@gmail.com>
2024-03-27 09:07:02 +0100

    register

--- register.php
+++ register.php
@@ -1,6 +1,42 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     include "conn.php";
+
+    $name = $email = $password = "";
+    $nameErr = $emailErr = $passwordErr = "";
+
+    if (empty($_POST["name"])) {
+        $nameErr = "Il nome è Vuoto";
+    } else {
+        $name = test_input($_POST["name"]);
+    }
+
+    if (empty($_POST["email"])) {
+        $emailErr = "L' email è Vuota";
+    } else {
+        $email = test_input($_POST["email"]);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Formato email non Valido";
+        }
+    }
+
+    if (empty($_POST["password"])) {
+        $passwordErr = "La password è Vuota";
+    } else {
+        $password = test_input($_POST["password"]);
+    }
+
+    if (empty($nameErr) && empty($emailErr) && empty($passwordErr)) {
+        echo "<p>Registration successful!</p>";
+    }
+
+    function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
 }
 ?>
 
@@ -20,19 +56,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <div class="container">
                     <div class="form-group">
                         <label for="exampleInputEmail1">Nome</label>
-                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nome">
+                        <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nome">
                     </div>
                     <div class="form-group">
                         <label for="exampleInputPassword1">Cognome</label>
-                        <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Cognome">
+                        <input type="text" name="cognome" class="form-control" id="exampleInputPassword1" placeholder="Cognome">
                     </div>
                     <div class="form-group">
                         <label for="exampleInputPassword1">Email</label>
-                        <input type="email" class="form-control" placeholder="Email">
+                        <input type="email" name="email" class="form-control" placeholder="Email">
                     </div>
                     <div class="form-group">
                         <label for="exampleInputPassword1">Telefono</label>
-                        <input type="text" class="form-control" placeholder="Telefono">
+                        <input type="text" name="password" class="form-control" placeholder="Telefono">
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
