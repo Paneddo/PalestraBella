d03b41b6dac63ebaa0fcf90a7c97c4255c606b1e
Paneddo <lorenzopanilorenzo@gmail.com>
2024-05-13 09:29:24 +0200

    corsi

--- corsi.php
+++ corsi.php
@@ -1,13 +1,9 @@
 <?php
-include "conn.php";
-
+include "utils.php";
 
 $corsi = array();
-
-include "conn.php";
-
-$stmt = mysqli_prepare($conn, "SELECT * FROM corso");
-
+$conn = getConnection();
+$stmt = mysqli_prepare($conn, "SELECT foto, nomeCorso as nome, prezzoOrario as prezzo FROM corso INNER JOIN utente ON utente.idUtente = corso.idIstruttore INNER JOIN tipologia ON tipologia.idTipologia = corso.idTipologia");
 mysqli_stmt_execute($stmt);
 
 $result = mysqli_stmt_get_result($stmt);
@@ -16,9 +12,7 @@ mysqli_close($conn);
 while ($row = mysqli_fetch_assoc($result)) {
     $corsi[] = $row;
 }
-
 ?>
-
 <!DOCTYPE html>
 <html lang="it">
 
@@ -36,14 +30,14 @@ while ($row = mysqli_fetch_assoc($result)) {
         <?php foreach ($corsi as $corso) : ?>
             <div class="course">
                 <div class="circular-square">
-                    <img src="istruttore3.jpg" alt="Immagine">
+                    <img src="./uploads/" <?= $corso['foto'] ?> alt="Immagine">
                 </div>
 
-                <h2><?= $corso['nomeCorso'] ?></h2>
+                <h2><?= $corso['nome'] ?></h2>
 
                 <p class="days">Lunedì 10.00-12.00</p>
                 <p class="days"> Mercoledì 10.00-12.00</p>
-                <p class="price">€<strong>10</strong>/ora</p>
+                <p class="price">€<strong><?= $corsi['prezzo'] ?></strong>/ora</p>
                 <a href="#" class="btn">Prenota</a>
             </div>
         <?php endforeach; ?>
